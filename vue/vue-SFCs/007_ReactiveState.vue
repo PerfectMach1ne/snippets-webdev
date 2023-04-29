f95ec57f369c49b65e7781ca73940a5afca64d35
PerfectMach1ne <75753483+PerfectMach1ne@users.noreply.github.com>
2023-04-29 23:55:02 +0200

    vłe

---
+++ vue/vue-SFCs/007_ReactiveState.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  data() {
+    return {
+      count: 1
+    }
+  },
+
+  mounted() {
+    // `this` refers to the component instance.
+    console.log(this.count);
+
+    // data can be mutated as well
+    this.count = 2;
+  }
+}
+</script>
+
+<template>
+  Count is {{ count }}
+</template>
