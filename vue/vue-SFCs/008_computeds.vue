42ef3111a7e6feccc9a183c57c3dc194a467c595
PerfectMach1ne <75753483+PerfectMach1ne@users.noreply.github.com>
2023-04-30 11:51:24 +0200

    vłe computed

---
+++ vue/vue-SFCs/008_computeds.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+  data() {
+    return {
+      author: {
+        name: 'John Doe',
+        books: [
+          'Vue 2 - Advanced Guide',
+          'Vue 3 - Basic Guide',
+          'Vue 4 - The Mystery'
+        ]
+      }
+    }
+  },
+  computed: {
+    publishedBooksMessage() {
+      return this.author.books.length > 0 ? 'Yes' : 'No';
+    },
+    now() { // This will NEVER update because Date.now() is not a reactive dependency
+      return Date.now();
+    }
+  }
+}
+</script>
+
+<template>
+  <p>Has published books:</p>
+  <span>{{ author.books.length > 0 ? 'Yes' : 'No' }}</span>
+  <br>
+  <span>{{ publishedBooksMessage }}</span>
+</template>
---
+++ vue/vue-SFCs/009_WritableComputed.vue
@@ -0,0 +1,38 @@
+<script>
+export default {
+  data() {
+    return {
+      firstName: 'John',
+      lastName: 'Doe',
+      test: 'a'
+    }
+  },
+  computed: {
+    fullName: {
+      // getter
+      get() {
+        return this.firstName + ' ' + this.lastName
+      },
+      // setter
+      set(newValue) {
+        // destructuring assignment syntax
+        [this.firstName, this.lastName] = newValue.split(' ')
+      }
+    }
+  },
+  methods: {
+    rock() {
+      this.test = 'RockDwayne Johnson';
+    }
+  }
+}
+</script>
+
+<template>
+  <p>"John" in 2015:</p>
+  <span>{{ fullName }}</span>
+  <p>Jane in 2020:</p>
+  <span>{{ fullName = 'Jane Doe' }}</span>
+  <br>
+  <span v-on:pointerover="rock">{{ test }}</span>
+</template>
