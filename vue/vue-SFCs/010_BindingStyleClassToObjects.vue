f234e3dfacd98625b84356b9c0cb359d0422744e
PerfectMach1ne <75753483+PerfectMach1ne@users.noreply.github.com>
2023-04-30 19:54:41 +0200

    vłe class style bindings

--- vue/vue-SFCs/010_BindingStyleClassToObjects.vue
+++ vue/vue-SFCs/010_BindingStyleClassToObjects.vue
@@ -15,6 +15,15 @@ export default {
       styleObject: {
         color: 'red',
         fontSize: '13px'
+      },
+      baseStyles: {
+        color: 'azure',
+        fontSize: '1.57em',
+        background: 'black',
+        width: 'fit-content'
+      },
+      overridingStyles: {
+        color: 'purple'
       }
     }
   }
@@ -35,4 +44,18 @@ export default {
   <div :style="styleObject">
     weh
   </div>
+  <!-- Binding to Arrays
+	We can bind :style to an array of multiple style objects.
+	These objects will be merged and applied to the same element:
+	-->
+  <div :style="[baseStyles, overridingStyles]">
+    bungus
+  </div>
+  <!-- You can provide an array of multiple (prefixed) values to a style property,
+ 	for example: -->
+  <div :style="{ display: ['-webkit-box', '-ms-flexbox', 'flex'] }">woo</div>
+  <!-- This will only render the last value in the array which the browser supports.
+ 	In this example, it will render display: flex for browsers that support the
+ 	unprefixed version of flexbox.
+	-->
 </template>
