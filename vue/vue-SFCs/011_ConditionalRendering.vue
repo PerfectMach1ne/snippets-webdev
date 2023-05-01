3123772beac19fbd5f3e72580bba3544901eef18
PerfectMach1ne <75753483+PerfectMach1ne@users.noreply.github.com>
2023-05-01 19:05:10 +0200

    v-if and stuff

---
+++ vue/vue-SFCs/011_ConditionalRendering.vue
@@ -0,0 +1,52 @@
+<script>
+export default {
+  data() {
+    return {
+      awesome: true,
+      awesomer: true,
+      type: '',
+      ok: true
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="wut">
+    <button @click="awesome = !awesome">spy cloak toggle</button>
+    <h1 v-if="awesome">vłe dżejes is sooo kuuuul.....</h1>
+    <button @click="awesomer = !awesomer">wazzaaap</button>
+    <h1 v-if="awesomer">Vłe.js 3 time</h1>
+    <h1 v-else>Vłe.js 2 jumpscare!!!!!</h1>
+
+    <input v-model="type">
+    <div v-if="type === 'A'">A</div>
+    <div v-else-if="type === 'B'">B</div>
+    <div v-else-if="type === 'C'">C</div>
+    <div v-else>Not A/B/C</div>
+
+    <button @click="ok = !ok">wee</button>
+    <template v-if="ok">
+      <h1>Tittle</h1>
+      <p>paragraph 1</p>
+      <p>poiragraph 2</p>
+    </template>
+  </div>
+</template>
+
+<style>
+.wut {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+}
+  
+button {
+  width: fit-content;
+}
+
+input {
+  width: 120px;
+}
+</style>
+<!-- what -->
