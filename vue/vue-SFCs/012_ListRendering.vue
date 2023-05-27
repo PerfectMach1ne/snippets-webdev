b88c30b2d6b3cbada0f805971ebd9aa2af228dd2
PerfectMach1ne <75753483+PerfectMach1ne@users.noreply.github.com>
2023-05-27 14:33:33 +0200

    Whatever this is

---
+++ vue/vue-SFCs/012_ListRendering.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  data() {
+  	return {
+	    parentMessage: 'Parent',
+    	items: [{ message: 'Foo' }, { message: 'Bar' }]
+  	}
+	}
+}
+</script>
+
+<template>
+	<li v-for="(item, index) in items">
+  	{{ parentMessage }} - {{ index }} - {{ item.message }}
+	</li>
+</template>
